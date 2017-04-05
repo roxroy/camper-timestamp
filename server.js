3585297f78382fe2398b1c10ad72276624108648
Roxroy <roxyclay@hotmail.com>
2017-04-05 00:35:40 +0000

    Add home page

---
+++ server.js
@@ -0,0 +1,48 @@
+var express = require('express')
+var path = require('path')
+var url = require('url');
+
+var app = express()
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'pug')
+app.set('views','public')
+
+
+var monthNames = ["January", "February", "March", "April", "May", "June",
+          "July", "August", "September", "October", "November", "December"
+        ];
+
+function formattedDate(now){
+    return monthNames[now.getMonth()] +' '+ 
+        now.getDate() +', ' +
+        now.getFullYear();
+};
+
+app.get('/', function(req, res) {
+    var now = new Date()
+    var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+    var result = {unix: Math.floor(now/1000), natural : formattedDate(now), url: fullUrl }
+    
+    res.render('index', result )
+})
+
+app.get('/:timeString', function(req, res) {
+     var dateString = req.params.timeString.trim();
+     var asDate = Date.parse(dateString);
+     var asNumber = Number(+dateString);
+     
+     var result = { unix : null,  natural : null }
+     
+     if (asNumber > 1) {
+         result.unix = asNumber;
+         result.natural = formattedDate(new Date(asNumber*1000));
+     } else  if (!isNaN(asDate)) {
+         result.natural = formattedDate(new Date(asDate));
+         result.unix = asDate/1000;
+     }
+     
+     res.json(result)
+})
+
+app.listen(process.env.PORT || 3000)
